554967859ba313143ac031067cf3ec6a3f4f96d5
Pardo <npdiaz99@gmail.com>
2026-03-13 16:23:04 -0500

    modified:   index.html modified:   script.js modified:   style.css

--- script.js
+++ script.js
@@ -61,6 +61,19 @@ document.addEventListener('DOMContentLoaded', () => {
             if(i === 0) div.click();
         });
     }
+        function openTab(evt, tabName) {
+        let i, tabcontent, tablinks;
+        tabcontent = document.getElementsByClassName("tab-content");
+        for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+        }
+        tablinks = document.getElementsByClassName("tab-btn");
+        for (i = 0; i < tablinks.length; i++) {
+            tablinks[i].className = tablinks[i].className.replace(" active", "");
+        }
+        document.getElementById(tabName).style.display = "block";
+        evt.currentTarget.className += " active";
+        }
 
     function actualizarVista(colorData) {
         fotoPrincipal.classList.add('fade-out');
